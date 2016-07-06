4747688528f7d35210ef97eca6844e6073616d26
Barry Yung <barry.yung@mail.utoronto.ca>
2016-07-06 14:09:17 -0400

    added 1.3.1.sql

---
+++ battleships/1.3.1.sql
@@ -0,0 +1,5 @@
+INSERT INTO classes(class,type,country,numguns,bore,displacement) VALUES ('Vittorio Veneto','bb','Italy',9,15,41000);
+
+INSERT INTO ships(name,class,launched) VALUES ('Vittorio Veneto','Vittorio Veneto',1940);
+INSERT INTO ships(name,class,launched) VALUES ('Vittorio Veneto','Italia',1940);
+INSERT INTO ships(name,class,launched) VALUES ('Vittorio Veneto','Roma',1942);
