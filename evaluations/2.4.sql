a229dc44f3e2735916a5f7e313b35c5a78fc8e23
njia95 <nathan.jia@mail.utoronto.ca>
2016-07-06 23:14:32 -0400

    finish 2.4

---
+++ evaluations/2.4.sql
@@ -0,0 +1,71 @@
+-- f on m
+select bty
+from
+    (select bty_f1lower as bty
+    from evaluations
+    where gender='male'
+    union all
+    select bty_f1upper as bty
+    from evaluations
+    where gender='male'
+    union all
+    select bty_f2upper as bty
+    from evaluations
+    where gender='male')
+group by bty
+order by count(*) desc
+limit 1;
+
+-- f on f
+select bty
+from
+    (select bty_f1lower as bty
+    from evaluations
+    where gender='female'
+    union all
+    select bty_f1upper as bty
+    from evaluations
+    where gender='female'
+    union all
+    select bty_f2upper as bty
+    from evaluations
+    where gender='female')
+group by bty
+order by count(*) desc
+limit 1;
+
+-- m on m
+select bty
+from
+    (select bty_m1lower as bty
+    from evaluations
+    where gender='male'
+    union all
+    select bty_m1upper as bty
+    from evaluations
+    where gender='male'
+    union all
+    select bty_m2upper as bty
+    from evaluations
+    where gender='male')
+group by bty
+order by count(*) desc
+limit 1;
+
+-- m on f
+select bty
+from
+    (select bty_m1lower as bty
+    from evaluations
+    where gender='female'
+    union all
+    select bty_m1upper as bty
+    from evaluations
+    where gender='female'
+    union all
+    select bty_m2upper as bty
+    from evaluations
+    where gender='female')
+group by bty
+order by count(*) desc
+limit 1;
