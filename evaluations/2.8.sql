f6e131b8babfc9b7a579063eb2d75dd6e4300079
njia95 <nathan.jia@mail.utoronto.ca>
2016-07-07 20:40:23 -0400

    update algorithm for 2.8

--- evaluations/2.8.sql
+++ evaluations/2.8.sql
@@ -1,14 +1,14 @@
 select sum(prof_eval) / count(*)
 from evaluations natural join (
     select prof_id, (
-        (case when language = 'english' then 1 else 0 end) +
-        (case when age = 45 then 1 else 0 end) +
-        (case when gender = 'female' then 1 else 0 end) + 
-        (case when bty_avg = 6 then 1 else 0 end) +
-        (case when ethnicity = 'non minority' then 1 else 0 end)
+        (case when language = 'english' then 0 else 1 end) +
+        (age - 45) * (age - 45) +
+        (case when gender = 'female' then 0 else 1 end) + 
+        (bty_avg - 6) * (bty_avg - 6) +
+        (case when ethnicity = 'non minority' then 0 else 1 end)
         ) as sim
     from evaluations
     group by prof_id
-    order by sim desc
+    order by sim
     limit 15
 );
